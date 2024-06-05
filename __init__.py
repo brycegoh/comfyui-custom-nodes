665d893b07c8abdb2cd188f2eab57376aad354f5
Bryce Goh <brycegohym@gmail.com>
2024-06-05 17:30:05 +0800

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -4,6 +4,7 @@ NODE_CLASS_MAPPINGS = {
     'MaskAreaComparisonSegment': MaskAreaComparisonSegment,
     'FillMaskedArea': FillMaskedArea,
     'OCRAndMask': DetectAndMask,
+    'CombineTwoImageIntoOne': CombineTwoImageIntoOne,
 }
 
 __all__ = ['NODE_CLASS_MAPPINGS']
